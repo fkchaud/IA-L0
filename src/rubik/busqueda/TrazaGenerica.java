c93ed81f38b4b4d5e1c096056bd55ff102bf5f39
Facundo Chaud <faku_imper@hotmail.com.ar>
2018-04-15 19:01:29 -0300

    Comentarios en TrazaGenerica. Falta implementarlo en otras clases.

--- src/rubik/busqueda/TrazaGenerica.java
+++ src/rubik/busqueda/TrazaGenerica.java
@@ -10,23 +10,38 @@ import java.util.LinkedList;
  * Inteligencia Artificial 2016
  */
 public class TrazaGenerica {
-     public TrazaGenerica (NodoBusqueda nodo){
-           System.out.println("\n/**** Accion: <estado,(costo),profundidad>    Por ejemplo Ui: <150,(2),2> ****/");
-           System.out.println("\n    Nodo raiz: " + toStringNodo(nodo));
-     }
-    //Este metodo debe llamarse despues de la expansion del nodo actual evaluado.
-
-    public void imprimirFinalIteracion(NodoBusqueda actual, LinkedList<NodoBusqueda> sucesores){
-        System.out.println("Nodo expanido: " + toStringNodo(actual) + " \n" + 
-                           "        Hijos: " + toStringListaNodos(sucesores));
+    public TrazaGenerica (NodoBusqueda nodo){
+          System.out.println("\n/**** Accion: <estado,(costo),profundidad>    Por ejemplo Ui: <150,(2),2> ****/");
+          System.out.println("\n    Nodo raiz: " + toStringNodo(nodo));
     }
-    //Este metodo debe llamarse antes de extraer un nodo de la lista abierta
 
+    /**
+     * Muestra el nodo expandido y sus hijos.
+     * Debe llamarse despues de la expansion del nodo actual evaluado.
+     * @param actual El nodo actual que está siendo expandido.
+     * @param sucesores Los hijos del nodo actual que ha sido expandido.
+     */
+    public void imprimirFinalIteracion(NodoBusqueda actual, LinkedList<NodoBusqueda> sucesores){
+        System.out.println("Nodo expandido: " + toStringNodo(actual) + " \n" + 
+                            "        Hijos: " + toStringListaNodos(sucesores));
+    }
+
+    /**
+     * Muestra los nodos de la lista abierta.
+     * Debe llamarse antes de extraer un nodo de la lista abierta.
+     * @param abierta La lista abierta con los nodos a mostrar.
+     */
     public void imprimirInicioIteracion(LinkedList<NodoBusqueda> abierta){
         System.out.println("\n" + 
                            "     Abierta: " + toStringListaNodos(abierta));
     }
     
+    /**
+     * Convierte un Nodo a un String.
+     * Muestra Etiqueta del operador: &lt;valor,(costo), profundidad&gt;
+     * @param nodo El nodo a convertir en String.
+     * @return La cadena a la que corresponde el nodo.
+     */
     private String toStringNodo(NodoBusqueda nodo){
             int valor = java.lang.Math.abs(nodo.getEstado().hashCode()/5000000);
             int costo = nodo.getCosto();
@@ -34,6 +49,13 @@ public class TrazaGenerica {
             return (nodo.getOperador()!=null?nodo.getOperador().getEtiqueta():"") + ": <" + valor + ",(" + costo + ")," + profundidad +">";
     }
  
+    /**
+     * Convierte una lista de Nodos en un String.
+     * Concatena una serie de Strings del método
+     * {@link #toStringNodo(rubik.busqueda.NodoBusqueda)}
+     * @param a Lista de nodos.
+     * @return String de nodos.
+     */
     private String toStringListaNodos(LinkedList<NodoBusqueda> a){
         String out = "";
         for(NodoBusqueda nodo : a){
