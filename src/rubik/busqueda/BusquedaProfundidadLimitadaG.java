5da5a916cf7b6698207fed6a1cc9cd9c23025825
Facundo Chaud <faku_imper@hotmail.com.ar>
2018-04-16 01:19:37 -0300

    Implementación BusquedaProfundidadLimitadaG To do: Gestionar desempates en BCU Arbol y Grafo

--- src/rubik/busqueda/BusquedaProfundidadLimitadaG.java
+++ src/rubik/busqueda/BusquedaProfundidadLimitadaG.java
@@ -12,34 +12,79 @@ import java.util.Vector;
 
 public class BusquedaProfundidadLimitadaG extends BusquedaGrafo implements Busqueda {
 
-  int profundidadLimite = 2;
-
-  public void setProfundidadLimite(int profundidadLimite) {
-    this.profundidadLimite = profundidadLimite;
-  }
-  public int getProfundidadLimite() {
-    return profundidadLimite;
-  }
-
-  public Vector<Operador> buscarSolucion(Estado inicial){
-
-    int profundidadActual = 0;
-    listaCerrada = new HashMap<Estado, NodoBusqueda>();
-    listaAbierta = new LinkedList<NodoBusqueda>();
- 
-    NodoBusqueda nodoSolucion = null;
-
-                
-     /*
-     * ......
+    /**
+     * Límite de la profundidad.
+     * El límite de la profundidad se settea en 26 porque es el llamado
+     * "Número de Dios", el número máximo de movimientos que son necesarios
+     * para resolver cualquier cubo rubik, con giros de 90°. (El número es 20
+     * para giros de 180°)
      */
+    int profundidadLimite = 26;
 
-    if(nodoSolucion == null) {
-      return new Vector<Operador>();
+    public void setProfundidadLimite(int profundidadLimite) {
+        this.profundidadLimite = profundidadLimite;
     }
-    else {
-      return encontrarCamino(nodoSolucion);
+    public int getProfundidadLimite() {
+        return profundidadLimite;
     }
-  }
 
+    @Override
+    public Vector<Operador> buscarSolucion(Estado inicial){
+        //Antes de comenzar la busqueda se contabiliza tiempo llamando metodo de la clase RendimientoBusqueda
+        reporteInicioBusqueda();
+        //inicializo las listas abierta y cerrada vacías
+        listaCerrada = new HashMap<Estado, NodoBusqueda>();
+        listaAbierta = new LinkedList<NodoBusqueda>();
+        //inicializa un booleano para el resultado de la búsqueda
+        boolean solucionEncontrada = false; 
+        //inicializo un nodo solución vacío (nulo)
+        NodoBusqueda nodoSolucion = null;
+        //inicializa el estado inicial como nodo incial
+        NodoBusqueda nodoActual = new NodoBusqueda(inicial,null,null);
+        nodoActual.setProfundidad(0);   
+        nodoActual.setCosto(0); 
+        //creo una Traza con el nodo raiz(actual)
+        traza = new TrazaGenerica(nodoActual);
+        //agrego el nodo inicial a la lista abierta
+        listaAbierta.add(nodoActual);
+        while(!solucionEncontrada) {
+          if(listaAbierta.isEmpty()) {
+            break;  
+          }
+          else {
+            //muestro estado de lista abierta al coienzo de cada interación
+            traza.imprimirInicioIteracion(listaAbierta);
+            //saco el primer elemento de la lista abierta
+            nodoActual = listaAbierta.pollFirst();
+            //Antes de evaluar si el nodo es solución contabilizo nodos explorados con la clase RendimientoBusqueda
+            reporteNodosExplorados();
+            //Si el estado del nodo actual no está en la lista cerrada...
+            if(!listaCerrada.containsKey(nodoActual.getEstado())) {
+                //Si el nodo actual es el objetivo
+                if (nodoActual.getEstado().esFinal()) {
+                    solucionEncontrada = true;
+                    nodoSolucion = nodoActual;
+                }
+                // si el estado actual no es objetivo lo expando (genero y pongo hijos)
+                else {
+                    listaCerrada.put(nodoActual.getEstado(), nodoActual);
+                    if (nodoActual.getProfundidad() < profundidadLimite) {
+                        listaAbierta.addAll(0, expandirNodo(nodoActual));
+                    }
+                }
+            }
+          }
+        }
+        // al terminar contabilizo nodos sobrantes con la clase RendimientoBusqueda
+        reporteNodosSobrantes(listaAbierta.size());
+        // Contabilizo tiempo al finalizar busqueda con la clase RendimientoBusqueda
+        reporteFinBusqueda();
+        
+        if(nodoSolucion == null) {
+          return new Vector<Operador>();
+        }
+        else {
+          return encontrarCamino(nodoSolucion);
+        }
+    }
 }
