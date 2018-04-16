30f850c112b57b95b795c04ec300fefc743822e0
Facundo Chaud <faku_imper@hotmail.com.ar>
2018-04-16 00:38:04 -0300

    Implementación de Busqueda Coste Uniforme. To do: Gestionar desempates

--- src/rubik/busqueda/BusquedaCosteUniforme.java
+++ src/rubik/busqueda/BusquedaCosteUniforme.java
@@ -12,25 +12,54 @@ import java.util.Vector;
 public class BusquedaCosteUniforme extends BusquedaArbol implements Busqueda {
 
     @Override
-  public Vector<Operador> buscarSolucion(Estado inicial){
-    abierta = new LinkedList<NodoBusqueda>();
-    NodoBusqueda nodoSolucion = null;
-      /*
-     * ......
-     */
-    if(nodoSolucion == null) {
-      return new Vector<Operador>();
+    public Vector<Operador> buscarSolucion(Estado inicial){
+        //Antes de comenzar se inicializa el tiempo para la medida de rendimiento
+        reporteInicioBusqueda();
+        abierta = new LinkedList<NodoBusqueda>();
+        boolean solucionEncontrada = false;
+        NodoBusqueda nodoSolucion = null;
+        NodoBusqueda nodoActual = new NodoBusqueda(inicial, null, null);
+        nodoActual.setCosto(0);
+        nodoActual.setProfundidad(0);
+        traza = new TrazaGenerica(nodoActual);
+        
+        while (!solucionEncontrada) {
+            if (abierta.isEmpty()) {
+                break;
+            } else {
+                traza.imprimirInicioIteracion(abierta);
+                nodoActual = getNodoMenorCostoFrontera();
+                reporteNodosExplorados();
+                if (nodoActual.getEstado().esFinal()) {
+                    solucionEncontrada = true;
+                    nodoSolucion = nodoActual;
+                } else {
+                    abierta.addAll(expandirNodo(nodoActual));
+                }
+            }
+        }
+        reporteNodosSobrantes(abierta.size());
+        reporteFinBusqueda();
+        if(nodoSolucion == null) {
+          return new Vector<Operador>();
+        }
+        else {
+          return encontrarCamino(nodoSolucion);
+        }
     }
-    else {
-      return encontrarCamino(nodoSolucion);
-    }
-  }
 
-  private NodoBusqueda getNodoMenorCostoFrontera() {
-     /*
-     * ......
-     */
-     return null;
+    private NodoBusqueda getNodoMenorCostoFrontera() {
+        NodoBusqueda nodoMenorCosto = abierta.get(0);
+        
+        for (NodoBusqueda n : abierta) {
+            if (n.getCosto() < nodoMenorCosto.getCosto()) {
+                nodoMenorCosto = n;
+            } else if (n.getCosto() == nodoMenorCosto.getCosto()) {
+                //TO DO gestionar desempates xD
+            }
+        }
+        
+        abierta.remove(nodoMenorCosto);
+        return nodoMenorCosto;
     }
-
 }
